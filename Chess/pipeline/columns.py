4a82ec27aac9c539b93bd542679f8fe9d30cb3d5
maks23438 <42715791+maxodk@users.noreply.github.com>
2024-05-19 21:19:33 +0300

    Delete Chess directory

--- Chess/pipeline/columns.py
+++
@@ -1,2 +0,0 @@
-y_column = ['winner'] # target variable
-X_columns = ['id', 'rated', 'created_at', 'last_move_at', 'turns', 'victory_status', 'increment_code', 'white_id', 'white_rating', 'black_id', 'black_rating', 'moves', 'opening_eco', 'opening_name', 'opening_ply']
